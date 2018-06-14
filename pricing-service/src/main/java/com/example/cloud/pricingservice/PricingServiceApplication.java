222bffaa28e8ba32fb4af21f926cad2aa76fa995
Marcin Piczkowski <piczmar@wp.pl>
2018-06-14 20:03:10 +0200

    added service discovery

--- pricing-service/src/main/java/com/example/cloud/pricingservice/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/example/cloud/pricingservice/PricingServiceApplication.java
@@ -2,11 +2,13 @@ package com.example.cloud.pricingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class PricingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PricingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PricingServiceApplication.class, args);
+    }
 }
